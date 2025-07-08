b47f276ceb85df24de14692e041c7053e1221e7b
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-07-08 21:41:43 +0600

    footer section setup

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "../Page/Shared/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../Page/Shared/Footer";
 
 const MainLayouts = () => {
   return (
     <>
       <Navbar />
+      <div className="min-h-[calc(100vh-108px)]">
+        <Outlet />
+      </div>
+      <Footer />
     </>
   );
 };
