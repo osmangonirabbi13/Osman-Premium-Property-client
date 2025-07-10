83c4e6254c9dee9060432af934641253fb2a090a
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-07-10 21:09:33 +0600

    reagister sign up implement

--- src/Page/Authentication/Register/Register.jsx
+++ src/Page/Authentication/Register/Register.jsx
@@ -2,23 +2,41 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link, useLocation, useNavigate } from "react-router";
 import axios from "axios";
-
+import useAuth from "../../../Hooks/useAuth";
+import toast from "react-hot-toast";
 const Register = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+  const { createUser, updateUserProfile } = useAuth();
   const [profilePic, setProfilePic] = useState("");
   const location = useLocation();
   const navigate = useNavigate();
   const from = location.state?.from || "/";
 
   const onSubmit = (data) => {
-    console.log("Form Data:", data);
-    console.log("Uploaded Image URL:", profilePic);
+    createUser(data.email, data.password)
+      .then(async (result) => {
+        // update user profile in firebase
+        const userProfile = {
+          displayName: data.name,
+          photoURL: profilePic,
+        };
+        updateUserProfile(userProfile)
+          .then(() => {
+            toast.success("Registration successful!");
 
-    navigate(from);
+            navigate(from);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   const handleImageUpload = async (e) => {
@@ -29,7 +47,7 @@ const Register = () => {
 
     const uploadUrl = `https://api.imgbb.com/1/upload?key=${
       import.meta.env.VITE_image_upload_key
-    }`;
+    };`;
 
     const res = await axios.post(uploadUrl, formData);
     setProfilePic(res.data.data.url);
@@ -57,7 +75,6 @@ const Register = () => {
               type="file"
               onChange={handleImageUpload}
               className="file-input file-input-neutral"
-              required
             />
 
             {/* Email Field */}
@@ -74,17 +91,28 @@ const Register = () => {
             <label className="label">Password</label>
             <input
               type="password"
-              {...register("password", { required: true, minLength: 6 })}
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters",
+                },
+                validate: {
+                  hasUpperCase: (value) =>
+                    /[A-Z]/.test(value) ||
+                    "Must include at least one uppercase letter",
+                  hasLowerCase: (value) =>
+                    /[a-z]/.test(value) ||
+                    "Must include at least one lowercase letter",
+                },
+              })}
               className="input"
               placeholder="Password"
             />
-            {errors.password?.type === "required" && (
-              <p className="text-red-500">Password is required</p>
-            )}
-            {errors.password?.type === "minLength" && (
-              <p className="text-red-500">
-                Password must be at least 6 characters
-              </p>
+
+            {/* Error Message Section */}
+            {errors.password && (
+              <p className="text-red-500 mt-1">{errors.password.message}</p>
             )}
 
             <button className="btn bg-[#CAEB66] text-black mt-4">
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import { router } from "./Routes/Route.jsx";
 import { RouterProvider } from "react-router";
 import AuthProvider from "./Provider/AuthProvider.jsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
+      <Toaster position="top-center" />
     </AuthProvider>
   </StrictMode>
 );
