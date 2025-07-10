80a71f726bb784a2430f2c538f105e0a00d24555
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-07-10 20:25:49 +0600

    Auth Provider setup

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,8 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
+import auth from "../Firebase/Firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updatePassword,
+  updateProfile,
+} from "firebase/auth";
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  // user create
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  // user Sign
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // user Update Profile
+  const updateUserProfile = (profileInfo) => {
+    return updateProfile(auth.currentUser, profileInfo);
+  };
+
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const changeUserPassword = (newPassword) => {
+    if (!auth.currentUser) return Promise.reject("No user signed in");
+    return updatePassword(auth.currentUser, newPassword);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unSubscribe();
+  }, []);
+  const authInfo = {
+    loading,
+    createUser,
+    signIn,
+    updateUserProfile,
+    logOut,
+    changeUserPassword,
+    user,
+    signInWithGoogle,
+  };
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
